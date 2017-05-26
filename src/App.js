99cb157a6c693599784172953345035a3509f3b5
Zain ul abidin <zainengineer@gmail.com>
2017-05-26 19:34:58 +0930

    trying separate component export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import FirstComp from './MyComponent'
+import * as Mycomp from './MyComponent'
+
 
 class App extends Component {
   render() {
@@ -10,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Mycomp.FirstComp/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class FirstComp extends Component {
+  render() {
+    return (
+      <div >
+        for  now static
+      </div>
+    );
+  }
+}
+export {FirstComp};
